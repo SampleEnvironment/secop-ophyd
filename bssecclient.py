febc02b0e9ec6043b5b9186dd9c247ed3a91f4f1
Peter Wegmann <peter.wegmann@helmholtz-berlin.de>
2023-04-20 10:12:14 +0200

    test

--- bssecclient.py
+++ bssecclient.py
@@ -5,7 +5,7 @@ import sys
 from AsyncSecopClient import AsyncSecopClient
 
 from SECoPDevices import SECoP_Node_Device
-from frappy.client import SecopClient
+from frappy-core.client import SecopClient
 from SECoPSignal import SECoPSignalR ,SECoPSignalRW
 
 import asyncio
@@ -20,7 +20,7 @@ async def main():
 
 
 
-    #testSig = SECoPSignalR(path=('cryo','value'),prefix='cryo:',secclient=secclient)
+    #testSig = SECoPSignalR(path=('cryo','value'),prefix='cryo:',secclient=secclient )
 
     #testSigRW = SECoPSignalRW(path=('cryo','target'),prefix='cryo:',secclient=secclient)
     
