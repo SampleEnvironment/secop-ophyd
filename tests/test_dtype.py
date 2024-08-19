b9ae94b120d7e4b813a452d48eb48854e5d8d0fb
Peter Wegmann <peter.wegmann@helmholtz-berlin.de>
2024-08-19 12:51:10 +0200

    dtype tests

--- tests/test_dtype.py
+++ tests/test_dtype.py
@@ -1,5 +1,5 @@
 import pytest
-from frappy.datatypes import ArrayOf, FloatRange, StringType, StructOf, TupleOf
+from frappy.datatypes import ArrayOf, FloatRange, StringType, IntRange,StructOf, TupleOf,BoolType,BLOBType,ScaledInteger,EnumType
 
 from secop_ophyd.util import NestedRaggedArray, SECoPdtype
 
@@ -31,3 +31,63 @@ def test_nested_ragged_arrays():
 
     with pytest.raises(NestedRaggedArray):
         SECoPdtype(ragged)
+
+
+def test_nested_arrays():
+
+    inner_dt_list = [
+        IntRange(),
+        FloatRange(),
+        ScaledInteger(1),
+        StringType(),
+        BoolType(),
+        BLOBType(),
+        EnumType('test',bla =0,blub = 1)
+    ]
+
+    dtype_list =[
+        'number',
+        'number',
+        'number',
+        'string',
+        'boolean',
+        'string',
+        'number'
+    ]
+
+    for innner_dt, dtype in zip(inner_dt_list,dtype_list):
+
+        arr_dt = ArrayOf(
+                    ArrayOf(
+                        ArrayOf(
+                            innner_dt,
+                            minlen=5,
+                            maxlen=5),
+                        minlen=5,
+                        maxlen=5),
+                    minlen=0,
+                    maxlen=5)
+        
+        sdtype = SECoPdtype(arr_dt)
+
+        assert sdtype.dtype == dtype
+
+
+
+    
+
+
+
+    ragged_arr = ArrayOf(
+            ArrayOf(
+                ArrayOf(
+                    IntRange(min=0 ,max=100),
+                    minlen=0,
+                    maxlen=5),
+                minlen=5,
+                maxlen=5),
+            minlen=5,
+            maxlen=5) 
+
+    with pytest.raises(NestedRaggedArray):
+        SECoPdtype(ragged_arr)                                      
