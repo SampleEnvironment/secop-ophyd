be043f3fac08da593829ec995fae08c61ccaedc8
Peter Wegmann <peter.wegmann@helmholtz-berlin.de>
2024-09-12 10:06:42 +0200

    rm nexus test

--- tests/test_Nexus.py
+++
@@ -1,9 +0,0 @@
-from secop_ophyd.SECoPDevices import SECoPNodeDevice
-
-
-async def test_node_nexus_struct(cryo_sim, cryo_node_internal_loop: SECoPNodeDevice):
-    generated_text = await cryo_node_internal_loop.generate_nexus_struct()
-
-    print(generated_text)
-
-    await cryo_node_internal_loop.disconnect_async()
